7ca612a0139714989108bc818ddf45671991c63c
Sito <sito8943@gmail.com>
2025-03-07 14:46:11 -0500

    apparition animations

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -5,10 +5,14 @@ function Home() {
   const { t } = useTranslation();
 
   return (
-    <main className="w-full h-full p-5 flex flex-col gap-5">
-      <h2 className="text-3xl text-center">{t("_pages:home.welcome")}</h2>
-      <p className="text-center text-balance">{t("_pages:home.description")}</p>
-      <p className="text-balance text-center">{t("_pages:home.tutorial")}</p>
+    <main className="w-full h-full p-5">
+      <div className="apparition flex flex-col gap-5">
+        <h2 className="text-3xl text-center">{t("_pages:home.welcome")}</h2>
+        <p className="text-center text-balance">
+          {t("_pages:home.description")}
+        </p>
+        <p className="text-balance text-center">{t("_pages:home.tutorial")}</p>
+      </div>
     </main>
   );
 }
--- src/Views/TimeToReal.jsx
+++ src/Views/TimeToReal.jsx
@@ -22,41 +22,45 @@ function TimeToReal() {
   }
 
   return (
-    <main className="flex flex-col gap-5 px-5 pt-5">
-      <h2 className="text-xl">{t("_pages:time-to-real.title")}</h2>
+    <main className="p-5">
+      <div className="apparition flex flex-col gap-5">
+        <h2 className="text-xl">{t("_pages:time-to-real.title")}</h2>
 
-      <form
-        className="flex flex-col items-center justify-start w-full gap-2"
-        onSubmit={(e) => {
-          e.preventDefault();
-          calculate();
-        }}
-      >
-        <input
-          className="input w-full"
-          onChange={(e) => setValue(e.currentTarget.value)}
-          placeholder={t("_pages:time-to-real.input.placeholder")}
-        />
-        <div className="w-full flex">
-          <button
-            type="submit"
-            className="button primary flex-1"
-            name={t("_accessibility:buttons.calculate")}
-            aria-label={t("_accessibility:ariaLabels.calculate")}
-          >
-            {t("_accessibility:buttons.calculate")}
-          </button>
-          <button
-            type="submit"
-            className="button flex-1"
-            name={t("_accessibility:buttons.clear")}
-            aria-label={t("_accessibility:ariaLabels.clear")}
-          >
-            {t("_accessibility:buttons.clear")}
-          </button>
-        </div>
-      </form>
-      <p className="w-full px-1">Resultado: {result}</p>
+        <form
+          className="flex flex-col items-center justify-start w-full gap-2"
+          onSubmit={(e) => {
+            e.preventDefault();
+            calculate();
+          }}
+        >
+          <input
+            className="input w-full"
+            onChange={(e) => setValue(e.currentTarget.value)}
+            placeholder={t("_pages:time-to-real.input.placeholder")}
+          />
+          <div className="w-full flex">
+            <button
+              type="submit"
+              className="button primary flex-1"
+              name={t("_accessibility:buttons.calculate")}
+              aria-label={t("_accessibility:ariaLabels.calculate")}
+            >
+              {t("_accessibility:buttons.calculate")}
+            </button>
+            <button
+              type="submit"
+              className="button flex-1"
+              name={t("_accessibility:buttons.clear")}
+              aria-label={t("_accessibility:ariaLabels.clear")}
+            >
+              {t("_accessibility:buttons.clear")}
+            </button>
+          </div>
+        </form>
+        <p className="w-full px-1">
+          {t("_pages:real-to-time.result")}: {result}
+        </p>
+      </div>
     </main>
   );
 }
