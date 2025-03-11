0d27381ef5727bf8d824d8b86da8d03863f8e4b6
Sito <sito8943@gmail.com>
2025-03-11 08:52:14 -0400

    fixed title

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -9,7 +9,11 @@ function Home() {
   return (
     <main className="w-full h-full p-5">
       <div className="apparition flex flex-col gap-5">
-        <h2 className="text-3xl text-center">{t("_pages:home.welcome")}</h2>
+        <h2 className="text-3xl text-center">
+          {t("_pages:home.welcome")}
+          <br />
+          {t("_pages:home.appName")}
+        </h2>
         <p className="text-center text-balance">
           {t("_pages:home.description")}
         </p>
